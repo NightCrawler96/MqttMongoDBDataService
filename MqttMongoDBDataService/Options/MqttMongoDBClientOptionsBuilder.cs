aa3688085c4dc721e780b8ec21c1257e0f4885fe
Grzegorz Jaworski <37083039+NightCrawler96@users.noreply.github.com>
2021-01-17 16:37:35 +0100

    Development (#4)
    * Added Serilog support and mqttclient
    
    * Registered MqttClientService
    
    * Fixed connection to broker
    
    * Connected to Mongo DB
    
    * Renamed Solution and project
    
    * Renamed directories
    
    * Updated docker ci
    
    * Updated dockerfile
    
    * Removed merge conflict
    
    * Fixed namespace errors

---
+++ MqttMongoDBDataService/Options/MqttMongoDBClientOptionsBuilder.cs
@@ -0,0 +1,15 @@
+﻿using MQTTnet.Client.Options;
+using System;
+
+namespace MqttMongoDBDataService.Options
+{
+    public class MqttMongoDBDataServiceOptionsBuilder : MqttClientOptionsBuilder
+    {
+        public IServiceProvider Provider { get; }
+
+        public MqttMongoDBDataServiceOptionsBuilder(IServiceProvider provider)
+        {
+            Provider = provider;
+        }
+    }
+}
